d5a3efc3392249633b850401505ebc0940b17f16
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-10-17 20:16:00 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/Views/Cesta.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Cesta />
     </View>
   );
 }
