4dfd017128d48a809f378e3862645f49b515b8e5
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-10-17 20:59:01 -0300

    View and new Components

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Cesta from './src/Views/Cesta.js';
+import { StatusBar, SafeAreaView } from 'react-native';
+import Cesta from './src/Pages/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar />
       <Cesta />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
